56ef8c84b617f496576cd0446c0c085805558957
Duan Wang <daniel.w2301@gmail.com>
2024-10-16 21:57:31 -0500

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,122 @@
+'use strict';
+
+function select(selector, scope = document) {
+    return scope.querySelector(selector);
+}
+
+function listen(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
+
+const userOs = select('.os');
+const userLanguage = select('.language');
+const userBrowser = select('.browser');
+const pageWidth = select('.width');
+const pageHeight = select('.height');
+const pageOrientation = select('.orientation');
+const batteryLevel = select('.level');
+const batteryStatus = select('.status');
+const networkStatus = select('.network');
+const networkColor = select('.box-three');
+
+// OS of SYSTEM
+
+function getOS() {
+    const userAgent = navigator.userAgent;
+    let os = "Unknown OS";
+
+    if (userAgent.indexOf("Win") !== -1) os = "Windows";
+    if (userAgent.indexOf("Mac") !== -1) os = "MacOS";
+    if (userAgent.indexOf("Android") !== -1) os = "Android";
+    if (userAgent.indexOf("like Mac") !== -1) os = "iOS";
+
+    return os;
+}
+
+userOs.innerText = `OS: ${getOS()}`;
+
+
+// Language of SYSTEM
+
+function getLanguage() {
+    return navigator.language || navigator.userLanguage;
+}
+
+userLanguage.innerText = `Language: ${getLanguage()}`;
+
+// Browser of SYSTEM
+
+function getBrowser() {
+    const userAgent = navigator.userAgent;
+    let browser = "Unknown Browser";
+
+    if (userAgent.indexOf("Chrome") !== -1) browser = "Chrome";
+    else if (userAgent.indexOf("Firefox") !== -1) browser = "Firefox";
+    else if (userAgent.indexOf("Safari") !== -1) browser = "Safari";
+    else if (userAgent.indexOf("MSIE") !== -1 || userAgent.indexOf("Trident") !== -1) browser = "Internet Explorer";
+    else if (userAgent.indexOf("Edge") !== -1) browser = "Edge";
+    return browser;
+}
+
+userBrowser.innerText = `Browser: ${getBrowser()}`;
+
+// Information of WINDOW
+
+function readWindow() {
+    pageWidth.innerText = `Width: ${window.innerWidth}px`;
+    pageHeight.innerText = `Height: ${window.innerHeight}px`;
+    
+    let orientation;
+    if (window.matchMedia("(orientation: landscape)").matches) {
+        orientation = 'Landscape';
+    } else {
+        orientation = 'Portrait';
+    }
+    pageOrientation.innerText = `Orientation: ${orientation}`;
+}
+
+listen('load', window, () => {
+    readWindow();
+});
+
+listen('resize', window,() => {
+    readWindow();
+});
+
+// Information of BATTERY
+function updateBatteryInfo(battery) {
+    batteryLevel.innerText = `Level: ${Math.round(battery.level * 100)}%`
+    batteryStatus.innerText = `Status: ${battery.charging ? "charging" : "idle"}`
+}
+
+if (navigator.getBattery) {
+    navigator.getBattery().then(function(battery) {
+        updateBatteryInfo(battery);
+        
+        battery.addEventListener('levelchange', function() {
+            updateBatteryInfo(battery);
+        });
+        battery.addEventListener('chargingchange', function() {
+            updateBatteryInfo(battery);
+        });
+    });
+} else {
+    batteryLevel.innerText = "not available";
+    batteryStatus.innerText = "not available";
+}
+
+// Information of NETWORK
+function updateNetworkStatus() {
+    const onlineStatus = navigator.onLine ? "Online" : "Offline";
+    networkStatus.innerText = `${onlineStatus}`;
+    if (navigator.onLine) {
+        networkColor.style.backgroundColor = '#00a84f';
+    } else {
+        networkColor.style.backgroundColor = '#a83500';
+    }
+}
+
+updateNetworkStatus();
+
+window.addEventListener('online', updateNetworkStatus);
+window.addEventListener('offline', updateNetworkStatus);
